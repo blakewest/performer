9c8f42412bab97589fb4ee65e9ff6a5c0c55d543
Blake West <bwest87@gmail.com>
2013-09-10 20:30:04 -0700

    added draft of PianoKey class file

--- src/PianoKey.js
+++ src/PianoKey.js
@@ -1,1 +1,48 @@
-pianoKey.js
+var PianoKey = function(boardInfo, note) {
+  //set up some convenience vars
+  var Black                    = boardInfo.KeyType.Black;
+  var keyType               = boardInfo.keyInfo[note].keyType;
+  var keyCenterPosX     = boardInfo.keyInfo[note].keyCenterPosX;
+  var keyUpSpeed         = boardInfo.keyUpSpeed;
+
+  //set up necessary components for making a Mesh.
+  var geometry, material, position;
+  if (keyType === Black) {
+    geometry = new THREE.CubeGeometry(boardInfo.blackKeyWidth, boardInfo.blackKeyHeight, boardInfo.blackKeyLength);
+    material   = new THREE.MeshPhongMaterial(boardInfo.blackKeyColor);
+    position   = new THREE.Vector3(keyCenterPosX, boardInfo.blackKeyPosY, boardInfo.blackKeyPosZ);
+  }else {
+    geometry = new THREE.CubeGeometry(boardInfo.whiteKeyWidth, boardInfo.whiteKeyHeight, boardInfo.whiteKeyLength);
+    material   = new THREE.MeshPhongMaterial(boardInfo.whiteKeyColor, emissive: 0x111111);
+    position   = new THREE.Vector3(keyCenterPosX, boardInfo.whiteKeyPosY, boardInfo.whiteKeyPosZ);
+  }
+
+  //make the key Mesh
+  this.model = new THREE.Mesh(geometry, material);
+  this.model.position.copy(position);
+
+  //set helper properties
+  this.keyUpSpeed = boardInfo.keyUpSpeed;
+  this.originalY = position.y;
+  this.pressedY = this.originalY - boardInfo.keyDip;
+
+  this.pressed = function() {
+    this.model.position.y = this.pressedY;
+    this.isPressed = true;
+  };
+
+  this.release = function() {
+    this.isPressed = false;
+  };
+
+  this.update = function() {
+    //this is really about making released notes edge up slowly, rather than quickly
+    if (this.model.position.y < this.originalY && this.pressed === false) {
+      //offset will keep getting smaller as the model's position gets raised by keyUpSpeed because update runs 60 times/second.
+      var offset = this.originalY - this.model.position.y;
+      this.model.position.y += Math.min(offset, keyUpSpeed);
+    }
+  };
+
+
+};
