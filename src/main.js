727db9445d83af0257c05de1c07ac5e4380548de
Blake West <bwest87@gmail.com>
2013-09-13 15:50:44 -0700

    one comment added from aseygul during code review

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 $(document).on('ready', function() {
-  var app = window.app = new App();
+  var app = window.app = new App(); //maybe put the whole app in a name space(like b), then if you need to refer to it, you can  refer to app as b.app
   app.initScene();
   app.initMIDI();
 });
@@ -7,3 +7,4 @@ $(document).on('ready', function() {
 var sendToUpload = function(file) {
   app.upload(file);
 };
+
