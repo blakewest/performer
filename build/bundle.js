10a9cab7f2da455f9dedc4319cfe21cca0468b30
Blake West <bwest87@gmail.com>
2013-09-17 13:17:48 -0700

    updated to include viterbi's algorithm

--- build/bundle.js
+++ build/bundle.js
@@ -323,123 +323,125 @@ mod.fingStretch = {
 
 },{}],4:[function(require,module,exports){
 var costDb = require('./CostAlgorithm.js').createCostDatabase();
-var chosenPath = [3];
-var oddScores = [];
-var allPaths = [];
 var curBest = Infinity;
-var bestPath = [];
+var endCap = [
+  {note: 'endCap'},
+  {note: 'endCap'},
+  {note: 'endCap'},
+  {note: 'endCap'},
+  {note: 'endCap'},
+];
 
-var findNextFinger = function(RHnotes, curNote, f1, totalScore) {
-  //take in the options, pick the lowest cost one directly infront.
-  var n1 = RHnotes[curNote];
-  var n2 = RHnotes[curNote+1];
-
-  if (n2 === undefined) {
-    return totalScore;
-  }
-  var min = 1000; //some arbitrary high number.
-  var bestOption;
-  for (var j = 1; j <= 5; j++) {
-    var key = n1.toString() + ',' + n2.toString() + ',' + f1.toString() + ',' + j.toString();
-    if (costDb[key] < min) {
-      min = costDb[key];
-      bestOption = j;
-    }
-  }
-  totalScore += min;
-  curNote += 1;
-  chosenPath.push([bestOption, min]);
-  if (min < 0) {
-    oddScores.push([bestOption, min]);
-  }
-  return findNextFinger(RHnotes, curNote, bestOption, totalScore);
+var Notes = {
+  0: 'C',
+  1: 'C#',
+  2: 'D',
+  3: 'Eb',
+  4: 'E',
+  5: 'F',
+  6: 'F#',
+  7: 'G',
+  8: 'G#',
+  9: 'A',
+  10: 'Bb',
+  11: 'B'
 };
 
-
-var findEveryPossiblePath = function(RHnotes, curNote, f1, curPath, totalScore) {
-  totalScore = totalScore || 0;
-  var n1 = RHnotes[curNote];
-  var n2 = RHnotes[curNote+1];
-
-  if (n2 === undefined) {
-    if (totalScore < curBest) {
-      bestPath.push(curPath);
-    }
-    // allPaths.push(totalScore);
-    return;
-  }
-  for (var f = 1; f <= 5; f++) {
-    var key = n1 + ',' + n2 + ',' + f1 + ',' + f;
-    var tempTotal = totalScore;
-    tempTotal += costDb[key];
-    curPath.push(f1);
-    findEveryPossiblePath(RHnotes, curNote+1, f, curPath, 
-    tempTotal);
-  }
-
+var makeNoteNode = function(noteNumber, fingerNumber) {
+  this.note = noteNumber;
+  this.finger = fingerNumber;
+  this.nodeScore = 0;
+  this.bestPrev = undefined;
 };
 
+var makeLayer = function(noteNumber) {
+  var layer = [];
+  for (var finger = 1; finger <= 5; finger++) {
+    var node = new makeNoteNode(noteNumber, finger);
+    layer.push(node);
+  }
+  return layer;
+};
+
+var makeRHNoteTrellis = function(midiData) {
+  var trellis = [];
+  //putting the endCap at the beginning is a convenience so we don't have to have special conditions in the traversal loop.
+  trellis.push(endCap);
+  for (var pair = 0; pair < midiData.length; pair++) {
+    var eventData = midiData[pair][0].event;
+    var note = eventData.noteNumber;
+    if (eventData.noteNumber >= 60 && eventData.subtype === 'noteOn') {
+      var layer = makeLayer(note);
+      trellis.push(layer);
+    }
+  }
+  return trellis;
+};
+
+var computeCost = function(n1,n2,f1,f2) {
+  if (n1 === 'endCap' || n2 === 'endCap') {
+    return 0;
+  }
+  var key = n1 + ',' + n2 + ',' + f1 + ',' + f2;
+  return costDb[key];
+};
+
+var findMin = function(layer) {
+  var minNode;
+  var minScore = Infinity;
+  for (var node = 0; node < layer.length; node++) {
+    if (layer[node].nodeScore < minScore) {
+      minScore = layer[node].nodeScore;
+      minNode = node;
+    }
+  }
+  return minNode;
+};
 
 module.exports.FingeringAlgorithm = function(midiData) {
-  //I want to construct an array of NoteOn events in the order they appear for the right hand. 
-  //I then need to walk across every possible option, and as I walk down the paths, I need to discard them as soon as I can. 
-    var RHnotes = [];
+   //this whole thing is an example of Viterbi's algorithm, if you're curious.
+    var RHnotes = makeRHNoteTrellis(midiData);
 
-    for (var pair = 0; pair < midiData.length; pair++) {
-      var eventData = midiData[pair][0].event;
-      var note = eventData.noteNumber
-      if (eventData.noteNumber >= 60 && eventData.subtype === "noteOn") {
-        RHnotes.push(note);
-
+    //traversing forward, computing costs and leaving our best path trail
+    for (var layer = 1; layer < RHnotes.length; layer++) {
+      for (var node1 = 0; node1 < 5 ; node1++) {
+        var min = Infinity;
+        for (var node2 = 0; node2 < 5; node2++) {
+          var curNode = RHnotes[layer][node1];
+          var prevNode = RHnotes[layer-1][node2];
+          var transCost = computeCost(prevNode.note, curNode.note, prevNode.finger, curNode.finger);
+          var totalCost = transCost + prevNode.nodeScore;
+          if (totalCost < min) {
+            min = totalCost;
+            curNode.nodeScore = totalCost;
+            curNode.bestPrev = node2;
+          }
+        }
       }
     }
-    //go through rh notes, start with thumb on first note. Always pick lowest cost option directly infront of you. adding to a total score 
-    var bestScore = findNextFinger(RHnotes, 0, 3, 0);
-    for (var f1 = 1; f1 <= 5; f1++) {
-      findEveryPossiblePath(RHnotes, 0, f1, [], 0);
+
+    /*going backwards, collecting the best path along the way.
+    the curNode variable is initialized to be the lowest score of the final layer.*/
+    var currentNode = findMin(RHnotes[RHnotes.length-1]);
+    var bestPath = [];
+    /*from this point, we put the finger for that node in the array, then we track back to it's
+    best previous node, record it's finger, and repeat till we get to the end.
+    also, we set the continuation condition to be greater than zero, because we don't actually want zero, 
+    since zero is just our start object.*/
+    for (var j = RHnotes.length-1; j > 0; j--) {
+      var nodeObj = RHnotes[j][currentNode];
+      var curFinger = nodeObj.finger;
+      var note = Notes[(nodeObj.note)%12];
+      bestPath.push([curFinger, note]);
+      currentNode = nodeObj.bestPrev;
     }
+    bestPath.reverse();
+    for (var i = 0; i < bestPath.length; i++) {
+      console.log('Note: ' + bestPath[i][1] + ' / Finger: ' + bestPath[i][0]);
+    }
+
     console.log(midiData);
-    console.log('Right Hand: ', RHnotes);
-    console.log('Best Path', bestPath);
 };
-
-
-
-
-
-
-
-
-
-        // if (eventData.subtype === 'noteOn') {
-        //   eventData.finger = fakeFingeringData[counter];
-        //   counter++;
-        // }
-
-
-        // if (eventData.noteNumber >= 60) {
-        //   RHnotes.push(note);
-        // }else {
-        //   // LHnotes.push(note);
-        // }
-
-
-    // var LHnotes = [];
-    // ["E", "E", "E", "E", "F", "G", "F", "G", "G", "F", "G", "E", "F", "D", "E", "C", "D", "C", "C", "D", "C", "E", "D", "E", "E", "D", "E", "D", "D", "E", "D", "E", "E", "F",
-    //  "E", "G", "F", "G", "G", "F", "G", "E", "F", "D", "E", "C", "D", "C", "C", "D", "C", "E", "D", "D", "E", "C", "D", "C", "C", "C"]
-
-    // var fakeFingeringData = [
-    //   3,3,3,3,4,5,4,5,5,4,5,3,4,2,3,1,2,1,1,2,1,3,2,3,3,2,3,2,2,3,2,3,3,4,3,5,4,5,5,4,5,3,4,2,3,1,2,1,1,2,1,3,2,2,3,1,2,1,1,1
-    // ];
-    // var counter = 0;
-
-
-
-
-
-
-
-
 
 },{"./CostAlgorithm.js":1}],5:[function(require,module,exports){
 var KeyboardDesign = require('./Visuals/Piano/KeyboardDesign.js').KeyboardDesign;
