ee09cc41956b31a6481c3509ceb1015961d36518
Blake West <bwest87@gmail.com>
2013-09-16 22:52:00 -0700

    have a terrible recursive solution. checking it in for the night

--- src/Algorithms/FingeringAlgorithm.js
+++ src/Algorithms/FingeringAlgorithm.js
@@ -1,6 +1,9 @@
 var costDb = require('./CostAlgorithm.js').createCostDatabase();
 var chosenPath = [3];
 var oddScores = [];
+var allPaths = [];
+var curBest = Infinity;
+var bestPath = [];
 
 var findNextFinger = function(RHnotes, curNote, f1, totalScore) {
   //take in the options, pick the lowest cost one directly infront.
@@ -29,6 +32,30 @@ var findNextFinger = function(RHnotes, curNote, f1, totalScore) {
 };
 
 
+var findEveryPossiblePath = function(RHnotes, curNote, f1, curPath, totalScore) {
+  totalScore = totalScore || 0;
+  var n1 = RHnotes[curNote];
+  var n2 = RHnotes[curNote+1];
+
+  if (n2 === undefined) {
+    if (totalScore < curBest) {
+      bestPath.push(curPath);
+    }
+    // allPaths.push(totalScore);
+    return;
+  }
+  for (var f = 1; f <= 5; f++) {
+    var key = n1 + ',' + n2 + ',' + f1 + ',' + f;
+    var tempTotal = totalScore;
+    tempTotal += costDb[key];
+    curPath.push(f1);
+    findEveryPossiblePath(RHnotes, curNote+1, f, curPath, 
+    tempTotal);
+  }
+
+};
+
+
 module.exports.FingeringAlgorithm = function(midiData) {
   //I want to construct an array of NoteOn events in the order they appear for the right hand. 
   //I then need to walk across every possible option, and as I walk down the paths, I need to discard them as soon as I can. 
@@ -44,13 +71,20 @@ module.exports.FingeringAlgorithm = function(midiData) {
     }
     //go through rh notes, start with thumb on first note. Always pick lowest cost option directly infront of you. adding to a total score 
     var bestScore = findNextFinger(RHnotes, 0, 3, 0);
+    for (var f1 = 1; f1 <= 5; f1++) {
+      findEveryPossiblePath(RHnotes, 0, f1, [], 0);
+    }
     console.log(midiData);
     console.log('Right Hand: ', RHnotes);
-    console.log('Total score: ', bestScore);
-    console.log('Chosen path : ', chosenPath);
-    console.log('oddScores : ', oddScores);
+    console.log('Best Path', bestPath);
 };
 
+/*  
+   OKKK... so you create the trellis, which is just slices of time. For monophonic stuff, these layers have 5 options. (one finger per note). For polyphonic stuff, 
+   it's a bit worse, but still reasonable. So your total trellis graph will have like... n*5, (or maybe n* 20 or something, if it's polyphonic) options, but then from there, 
+   it's linear. So this reduces the time complexity to linear!!!
+   And then for each node in the graph, you find the best option up to that point, which is the sum of the best option before, plus transition cost of each option
+   at that node. See the white board. 
 
 
 
