d8ea320e7a142009683a427e0c92e5a71d887f87
Blake West <bwest87@gmail.com>
2013-09-16 17:47:12 -0700

    minor changes to bring in the fingering algorithm part

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ var KeyboardDesign = require('./Visuals/Piano/KeyboardDesign.js').KeyboardDesign
 var Keyboard = require('./Visuals/Piano/Keyboard.js').Keyboard;
 var RightHand = require('./Visuals/Hand/RightHand.js').RightHand;
 var Scene = require('./Visuals/Scene.js').Scene;
+var createDb = require('./Algorithms/CostAlgorithm').createCostDatabase;
+var fingeringAlgo = require('./Algorithms/FingeringAlgorithm.js').FingeringAlgorithm;
 
 module.exports.App = function() {
   //instantiate piano and hand
@@ -19,22 +21,22 @@ module.exports.App = function() {
   var _this = this;
 
   //this is the callback that fires every time the MIDI.js library 'player' object registers a MIDI event of any kind.
-  this.player.addListener(function(data) {
-    var rightHand = _this.rightHand;
-    var NOTE_ON = 144;
-    var NOTE_OFF = 128;
-    var note = data.note;
-    var message = data.message;
-    var finger = data.finger;
+  // this.player.addListener(function(data) {
+  //   var rightHand = _this.rightHand;
+  //   var NOTE_ON = 144;
+  //   var NOTE_OFF = 128;
+  //   var note = data.note;
+  //   var message = data.message;
+  //   var finger = data.finger;
 
-    if (message === NOTE_ON) {
-      _this.keyboard.press(note);
-      rightHand.press(finger);
-    }else if(message === NOTE_OFF) {
-      _this.keyboard.release(note);
-      rightHand.release(finger);
-    }
-  });
+  //   if (message === NOTE_ON) {
+  //     _this.keyboard.press(note);
+  //     rightHand.press(finger);
+  //   }else if(message === NOTE_OFF) {
+  //     _this.keyboard.release(note);
+  //     rightHand.release(finger);
+  //   }
+  // });
 
   this.player.setAnimation({
     delay: 20,
@@ -129,61 +131,8 @@ module.exports.App = function() {
   };
 
   this.fingeringAlgorithm = function() {
-    var midiData = _this.player.data;
-    var notes = {
-      0: 'C',
-      1: 'Db',
-      2: 'D',
-      3: 'Eb',
-      4: 'E',
-      5: 'F',
-      6: 'Gb',
-      7: 'G',
-      8: 'Ab',
-      9: 'A',
-      10: 'Bb',
-      11: 'B'
-    };
-    var RHnotes = [];
-    var LHnotes = [];
-    // ["E", "E", "E", "E", "F", "G", "F", "G", "G", "F", "G", "E", "F", "D", "E", "C", "D", "C", "C", "D", "C", "E", "D", "E", "E", "D", "E", "D", "D", "E", "D", "E", "E", "F",
-    //  "E", "G", "F", "G", "G", "F", "G", "E", "F", "D", "E", "C", "D", "C", "C", "D", "C", "E", "D", "D", "E", "C", "D", "C", "C", "C"]
-
-    // ["E", "E", "E", "E", "F", "G", "F", "G", "G", "F", "G", "E", "F", "D", "E", "C", "D", "C", "C", "D", "C", "E", "D", "E", "E", "D", "E", "D", "D", "E", "D", "E", "E", "F", 
-    //  "E", "G", "F", "G", "G", "F", "G", "E", "F", "D", "E", "C", "D", "C", "C", "D", "C", "E", "D", "D", "E", "C", "D", "C", "C", "C"]
-
-    var fakeFingeringData = [
-      3,3,3,3,4,5,4,5,5,4,5,3,4,2,3,1,2,1,1,2,1,3,2,3,3,2,3,2,2,3,2,3,3,4,3,5,4,5,5,4,5,3,4,2,3,1,2,1,1,2,1,3,2,2,3,1,2,1,1,1
-    ];
-    var counter = 0;
-    for (var pair = 0; pair < midiData.length; pair++) {
-      var eventData = midiData[pair][0].event;
-      // eventData.subtype === 'noteOn' || eventData.subtype === 'noteOff' && 
-      if (eventData.noteNumber >= 60) {
-        var note = notes[eventData.noteNumber%12];
-        RHnotes.push(note);
-        if (eventData.subtype === 'noteOn' || eventData.subtype === 'noteOff') {
-          eventData.finger = fakeFingeringData[counter];
-          counter++;
-        }
-
-
-        // if (eventData.noteNumber >= 60) {
-        //   RHnotes.push(note);
-        // }else {
-        //   // LHnotes.push(note);
-        // }
-      }
-      // if (eventData.subtype === 'noteOff') {
-      //   eventData.finger = 0;
-      // }
-    }
-
-    console.log(midiData);
-    console.log('Right Hand: ', RHnotes);
-    console.log('Left Hand: ', LHnotes);
+    fingeringAlgo(_this.player.data);
   };
-
 };
 
 
