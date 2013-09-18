75ed3cb68ce25a34623fd825c936f5835b6a6a95
Blake West <bwest87@gmail.com>
2013-09-17 21:24:37 -0700

    have draft of adding polyphonic layers. Not quite functional though

--- src/Algorithms/FingeringAlgorithmHelpers.js
+++ src/Algorithms/FingeringAlgorithmHelpers.js
@@ -1,7 +1,14 @@
 var costDb = require('./CostAlgorithm.js').createCostDatabase();
 var mod = module.exports;
 
-mod.notes = {0: 'C', 1: 'C#',2: 'D',3: 'Eb',4: 'E',5: 'F',6: 'F#',7: 'G',8: 'G#',9: 'A',10: 'Bb',11: 'B'};
+mod.notes = {0:'C', 1:'C#', 2:'D', 3:'Eb', 4:'E', 5:'F', 6:'F#', 7:'G', 8:'G#', 9:'A', 10:'Bb', 11:'B'};
+
+var polyPhonicOptions = {
+  '2': [[1,2], [1,3], [1,4], [1,5], [2,3], [2,4], [2,5], [3,4], [3,5], [4,5]],
+  '3': [[1,2,3], [1,2,4], [1,2,5], [1,3,4], [1,3,5], [1,4,5], [2,3,4], [2,3,5], [2,4,5], [3,4,5]],
+  '4': [[1,2,3,4], [1,2,3,5], [1,2,4,5], [1,3,4,5], [2,3,4,5]],
+  '5': [[1,2,3,4,5]]
+};
 
 var endCap = [
   {note: 'endCap'},
@@ -11,9 +18,10 @@ var endCap = [
   {note: 'endCap'},
 ];
 
-var makeNoteNode = function(noteNumber, fingerNumber) {
-  this.note = noteNumber;
-  this.finger = fingerNumber;
+var makeNoteNode = function(notes, fingers) {
+  //the notes and fingers property can have either one or multiple notes. 
+  this.notes = notes;
+  this.fingers = fingers;
   this.nodeScore = 0;
   this.bestPrev = undefined;
 };
@@ -27,7 +35,18 @@ var makeLayer = function(noteNumber) {
   return layer;
 };
 
+var makePolyPhonicLayer = function(noteNumbers) {
+  var layer = [];
+  var options = polyPhonicOptions[noteNumbers.length]; // this grabs the appropriate list of options. 
+  for (var i = 0; i < options.length; i++) {
+    var fingerChoice = options[i];
+    var node = makeNoteNode(noteNumbers, fingerChoice);
+    layer.push(node);
+  }
+};
+
 mod.makeRHNoteTrellis = function(midiData) {
+  var curPlaying = [];
   var trellis = [];
   //putting the endCap at the beginning is a convenience so we don't have to have special conditions in the traversal loop.
   trellis.push(endCap);
@@ -41,6 +60,48 @@ mod.makeRHNoteTrellis = function(midiData) {
   }
   return trellis;
 };
+
+mode.makeRHPolyNoteTrellis = function(midiData) {
+  //i'll need a 'currentlyPlaying' array, a 'newLayer' array, and a 'lastwasOn' variable that tracks if the last event was a noteOn or noteOff
+  //noteOn event comes in, put the note in currentlyPlaying
+  //noteOff event comes in... 
+  // if 'lastwasOn' is true, then place all currentlyPlaying into newLayer, and remove the noteOff from currentlyPlaying
+  // if 'lastwasOn' is false, then remove that note from currentlyPlaying, and don't push anything to newLayer.
+  var curPlaying = [];
+  var lastWasOn = false;
+  var trellis = [];
+
+  for (var pair = 0; pair < midiData.length; pair++) {
+    var eventData = midiData[pair][0].event;
+    var note = eventData.noteNumber;
+    if (note >= 60 && eventData.subtype === 'noteOn') {
+      curPlaying.push(note);
+      lastWasOn = true;
+    }
+    if (note >= 60 && eventData.subtype === 'noteOff') {
+      if (lastWasOn) {
+        var node = makePolyPhonicLayer(curPlaying);
+        trellis.push(node);
+      }else {
+        var notePlace = curPlaying.indexOf(note);
+        curPlaying.splice(notePlace, 1);
+        lastWasOn = false;
+      }
+    }
+  }
+  return trellis;
+};
+
+mod.makeRHnotesData = function(midiData) {
+  var noteData = [];
+  for (var pair = 0; pair < midiData.length; pair++) {
+    var eventData = midiData[pair][0].event;
+    if (eventData.noteNumber >= 60 && eventData.subtype === 'noteOn') {
+      noteData.push(midiData[pair]);
+    }
+  }
+  return noteData;
+}
 
 mod.computeCost = function(n1,n2,f1,f2) {
   if (n1 === 'endCap' || n2 === 'endCap') {
