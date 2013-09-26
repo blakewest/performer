f3f8783f3b17f1d69959748ab0f9ab9aae7a07a3
Blake West <bwest87@gmail.com>
2013-09-26 16:55:32 -0700

    took out unnecessary comments and such

--- server.js
+++ server.js
@@ -12,20 +12,6 @@ server.configure(function() {
 //calls our routes file and passes the server and config object.
 require('./routes.js').routes(server, config);
 
-//save pre-computed song files in the DB with following properties:
-  //title
-  //artist
-  //pre-computed data
-
-//on client side
-  //have logic to set replayer data to the stuff we just got from server. 
-  //no need for local storage. 
-  //just have a script that lazy loads all songs from the DB.
-
-//server side
-  //to get files on server, we could save the finished root.data to a variable, and then send a post-request with stringified data.
-  //with a copy of the data. then just manually add a name/artist property later. *do it the dumb way for now*
-
 var port = process.env.PORT || config.port || 3000;
 
 console.log('server is listening on port ', port);
