863eb3f3990cab26023131410d75681375a36942
Blake West <bwest87@gmail.com>
2013-09-26 16:51:08 -0700

    implemented right chain for adding songs to db, and serving up files when they're clicked on the site

--- routes.js
+++ routes.js
@@ -5,59 +5,56 @@ var fs = require('fs');
 var db = mongojs('mongodb://localhost/performer');
 var collection = db.collection('songs');
 
+/*
+!!!HEY, WHAT ABOUT JUST STORING THE BEST PATH OBJECT AND NOT THE WHOLE THING>
+THEN JUST PASS THAT, AND RUN DISTRIBUTE PATH AS NORMAL!!!!
+*/
+
 module.exports.routes = function(server, config) {
   // require('./config/db.js').db(server, config);
   // var Song = mongoose.model('Song', 'songs');
   server.use(express.bodyParser());
 
+  //put the whole object in the db. This will contain a title, artist, and best path obj. (computed fingering data);
   server.post('/upload', function(req,res) {
-    //TO DO: 
-    //put the song data in the database
-    // console.log('replayer data at upload time is... ', JSON.stringify(req.body.songData));
     res.writeHead(200);
-    // var newSong = new Song({
-    //   title: req.body.title,
-    //   artist: req.body.artist,
-    //   songData: req.body.songData.toString(),
-    // });
-    // newSong.save();
     console.log('we theoretically just added a new song');
     collection.insert(req.body);
     res.end();
   });
 
-  server.get('/bwv784.mid', function(req,res) {
+  //actually sends back the midi files we have stored on the server.
+  server.get('/files/:song', function(req,res) {
     res.writeHead(200);
-    fs.readFile('./TestMidiFiles/bwv784.mid', function(err, data) {
-      // var stringified = data.toString();
+    fs.readFile('./TestMidiFiles/' + req.params.song, function(err, data) {
       res.end(data);
     });
   });
 
-  server.get('/songname', function(req,res) {
-    //TO DO:
-    //send song data back from the database
-    console.log('songname GET is called');
-    collection.findOne({title: 'Last Modified: March 18, 1997'}, function(err, doc) {
-      var stringData = JSON.stringify([doc.songData, doc.replayerData]);
-      res.write(stringData);
-      res.end();
+  //send back all best path objs we have in one request, so we don't need to make multiple requests as we get new songs. 
+  server.get('/getAllPaths', function(req,res) {
+    console.log('get All Paths is called');
+    collection.find(function(err, docs) {
+      var stringData = JSON.stringify(docs);
+      res.end(stringData);
     });
   });
 
-  server.get('/songname2', function(req,res) {
-    //TO DO:
-    //send song data back from the database
-    console.log('songname GET2 is called');
+  // //send back all best path objs we have in one request, so we don't need to make multiple requests as we get new songs. 
+  // server.get('/getPath/:songname', function(req,res) {
+  //   collection.find({ songname: req.params.songName }, function(err, doc) {
+  //     res.json(doc);
+  //   });
+  // });
+
+  //hand back requested song name, so the XHTTP object on client can use that to request the real file. 
+  //We don't just send back the file straight up because the XHTTP has special functions it uses 
+  server.get('/songs/:song', function(req,res) {
+    console.log(req.params.song + ' is getting asked for');
     res.writeHead(200, {
       'Content-Type' : 'text/plain'
     });
-    res.end('bwv784.mid');
-    // collection.findOne({title: 'Last Modified: March 18, 1997'}, function(err, doc) {
-    //   var stringData = JSON.stringify([doc.songData, doc.replayerData]);
-    //   res.write(stringData);
-    //   res.end();
-    // });
+    res.end(req.params.song + '.mid');
   });
 };
 
