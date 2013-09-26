67572888152fdfc1bff025fc91774b026f521928
Blake West <bwest87@gmail.com>
2013-09-26 16:58:59 -0700

    added call at the beginning to get all song data.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 var App = require('./App.js').App;
 $(document).on('ready', function() {
   var app = window.app = new App(); //maybe put the whole app in a name space(like b), then if you need to refer to it, you can  refer to app as b.app
+  $.ajax({
+    url: '/getAllPaths',
+    // dataType: 'text',
+    success: function(data) {
+      var allPaths = JSON.parse(data);
+      console.log('data after GET request...', allPaths);
+      app.preComputed = allPaths;
+    }
+  });
+  console.log('app Pre Computed = ', app.preComputed);
   app.initScene();
   app.initMIDI();
   app.initPlayControls($('.playerArea'), app);
