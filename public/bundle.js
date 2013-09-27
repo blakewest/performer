c062047c74bd69830ac1310a16f546d1577a9023
Blake West <bwest87@gmail.com>
2013-09-26 21:00:46 -0700

    added half-speed button

--- public/bundle.js
+++ public/bundle.js
@@ -334,7 +334,6 @@ var helpers = require('./FingeringAlgorithmHelpers.js');
 
 module.exports.FingeringAlgorithm = function(midiData) {
  //this whole thing is an example of Viterbi's algorithm, if you're curious.
-
   var dataWithStarts = helpers.addStartTimes(midiData);
   //this checks if we already have the best path data for that song on the client.
   for (var i = 0; i < app.preComputed.length; i++) {
@@ -812,11 +811,11 @@ module.exports.App = function() {
     }
   });
 
-  this.loadMidiFile = function(midiFile, callback) {
+  this.loadMidiFile = function(midiFile, newStartPercent) {
     var _this = this;
     //just calls loadFile from the MIDI.js library, which kicks off a few calls to parse the MIDI data.
     this.player.loadFile(midiFile, function() {
-      _this.playControls.play();
+      _this.playControls.setCurrentTIme(newStartPercent);
     });
   };
 
@@ -865,8 +864,6 @@ module.exports.App = function() {
   this.fingeringAlgorithm = function() {
     fingeringAlgo(_this.player.data);
   };
-
-  this.preComputed = [];
 };
 
 
@@ -931,14 +928,12 @@ module.exports.PlayControls = function(container, app) {
   var $songListContainer = $('.player-songListContainer', this.$container);
   var $controlsContainer = $('.player-controls', this.$container);
   var $progressContainer = $('.player-progress-container', this.$container);
-  console.log($progressContainer);
 
   var $progressBar = $('.player-progress-bar', this.$container);
   var $progressText = $('.player-progress-text', this.$container);
   var $songList = $('.player-songList', this.$container);
   var $song = $('.song', this.$container);
-
-  console.log($progressBar);
+  var $tempoChanger = $('.tempoChanger', this.$container);
 
   var $playBtn = $('.player-playBtn', this.$container);
   var $pauseBtn = $('.player-pauseBtn', this.$container);
@@ -968,14 +963,15 @@ module.exports.PlayControls = function(container, app) {
     if ($target.is('li')) {
       // var $songList = $('li', _this.songList);
       var trackName = $target.text();
-      app.player.stop();
+      // app.player.stop();
       _this.playing === false;
       app.currentSong = trackName;
+      console.log(app.currentSong);
       $.ajax({
         url: '/songs/'+trackName,
         dataType: 'text',
         success: function(data) {
-          app.loadMidiFile(data);
+          app.loadMidiFile(data, 0);
         }
       });
     }
@@ -992,15 +988,30 @@ module.exports.PlayControls = function(container, app) {
     _this.setCurrentTIme(progressPercent);
   });
 
+  $tempoChanger.click(function() {
+    console.log('timewarp function getting called');
+    app.player.timeWarp = 2;
+    var trackName = app.currentSong;
+    $.ajax({
+      url: '/songs/'+trackName,
+      dataType: 'text',
+      success: function(data) {
+        var currentPercent = app.player.currentTime/app.player.endTime;
+        app.loadMidiFile(data, ($progressBar.width() / $progressContainer.width()) );
+      }
+    });
+  })
+
   // $container.on('mousewheel', function(event) {
   //     event.stopPropagation();
   //   });
 
   this.play = function() {
+    console.log('play is getting called');
     $playBtn.hide();
     $pauseBtn.show();
     _this.playing = true;
-    app.player.start();
+    app.player.resume();
   };
 
   this.resume = function() {
@@ -1037,6 +1048,7 @@ module.exports.PlayControls = function(container, app) {
   };
 
   this.setCurrentTIme = function(progressPercent) {
+    console.log('set current time is getting called');
     var currentTime = app.player.endTime * progressPercent;
     app.player.currentTime = currentTime;
     setTimeout(app.player.resume, 10);
