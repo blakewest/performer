08358b3a31dcffba6a50ebfc768d434ddd5c8625
Blake West <bwest87@gmail.com>
2013-09-28 17:24:43 -0700

    added some code that will hopefully make so I can use a 3rd party mongo service in production

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var rootPath = path.join(__dirname + '..');
-
+var production = process.env.NODE_ENV;
 module.exports = {
   development: {
     db: 'mongodb://localhost/performer',
     root: rootPath,
     port: 3000
+  },
+  production: {
+    db:'mongodb://myremotedb/performer'
   }
 };
