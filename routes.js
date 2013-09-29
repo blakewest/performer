96eb1d2ff0dd63f3d92403cf2354d2ee71981722
Blake West <bwest87@gmail.com>
2013-09-28 17:23:58 -0700

    took out old comments and set it up so I could use a 3rd party production mongo service

--- routes.js
+++ routes.js
@@ -2,23 +2,15 @@
 var mongojs = require('mongojs');
 var express = require('express');
 var fs = require('fs');
-var db = mongojs('mongodb://localhost/performer');
-var collection = db.collection('songs');
-
-/*
-!!!HEY, WHAT ABOUT JUST STORING THE BEST PATH OBJECT AND NOT THE WHOLE THING>
-THEN JUST PASS THAT, AND RUN DISTRIBUTE PATH AS NORMAL!!!!
-*/
 
 module.exports.routes = function(server, config) {
-  // require('./config/db.js').db(server, config);
-  // var Song = mongoose.model('Song', 'songs');
+  var db = mongojs(config.db);
+  var collection = db.collection('songs');
   server.use(express.bodyParser());
 
   //put the whole object in the db. This will contain a title, artist, and best path obj. (computed fingering data);
   server.post('/upload', function(req,res) {
     res.writeHead(200);
-    console.log('we theoretically just added a new song');
     collection.insert(req.body);
     res.end();
   });
@@ -33,24 +25,15 @@ module.exports.routes = function(server, config) {
 
   //send back all best path objs we have in one request, so we don't need to make multiple requests as we get new songs. 
   server.get('/getAllPaths', function(req,res) {
-    console.log('get All Paths is called');
     collection.find(function(err, docs) {
       var stringData = JSON.stringify(docs);
       res.end(stringData);
     });
   });
 
-  // //send back all best path objs we have in one request, so we don't need to make multiple requests as we get new songs. 
-  // server.get('/getPath/:songname', function(req,res) {
-  //   collection.find({ songname: req.params.songName }, function(err, doc) {
-  //     res.json(doc);
-  //   });
-  // });
-
   //hand back requested song name, so the XHTTP object on client can use that to request the real file. 
   //We don't just send back the file straight up because the XHTTP has special functions it uses 
   server.get('/songs/:song', function(req,res) {
-    console.log(req.params.song + ' is getting asked for');
     res.writeHead(200, {
       'Content-Type' : 'text/plain'
     });
