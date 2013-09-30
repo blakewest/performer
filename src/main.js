b08365ab31623f013e526d4946d3346349deca01
Blake West <bwest87@gmail.com>
2013-09-29 21:04:38 -0700

    removed old console logs and increased wait time for auto-play. sometimes sound wasn't fully loaded

--- src/main.js
+++ src/main.js
@@ -3,20 +3,18 @@ $(document).on('ready', function() {
   var app = window.app = new App(); //maybe put the whole app in a name space(like b), then if you need to refer to it, you can  refer to app as b.app
   $.ajax({
     url: '/getAllPaths',
-    // dataType: 'text',
     success: function(data) {
       var allPaths = JSON.parse(data);
-      console.log('data after GET request...', allPaths);
       app.preComputed = allPaths;
     }
   });
-  console.log('app Pre Computed = ', app.preComputed);
   app.initMIDI();
   app.initPlayControls($('.main-container'), app);
   app.initScene();
+  //Sound takes a while to load, so we use the setTimeout to ensure it's ready.
   setTimeout(function() {
     $($('.player-songList > li')[0]).trigger('click');
-  }, 1500);
+  }, 2000);
 });
 
 
