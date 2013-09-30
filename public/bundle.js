397b9cc488c84db78761061f958b072b7c78bc88
Blake West <bwest87@gmail.com>
2013-09-30 11:36:47 -0700

    updated order

--- public/bundle.js
+++ public/bundle.js
@@ -910,8 +910,8 @@ $(document).on('ready', function() {
       app.preComputed = allPaths;
     }
   });
-  app.initScene();
   app.initPlayControls($('.main-container'), app);
+  app.initScene();
   //Sound takes a while to load, so we use the setTimeout to ensure it's ready.
   setTimeout(function() {
     $($('.player-songList > li')[0]).trigger('click');
