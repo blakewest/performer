10141792288b7a6354fa4548adef258455c2d556
Blake West <bwest87@gmail.com>
2013-09-30 14:29:07 -0700

    added new google analytics code, and put more things in the call back of the first ajax call.

--- public/bundle.js
+++ public/bundle.js
@@ -909,13 +909,14 @@ $(document).on('ready', function() {
       success: function(data) {
         var allPaths = JSON.parse(data);
         app.preComputed = allPaths;
+        app.initPlayControls($('.main-container'), app);
+        //Sound takes a while to load, so we use the setTimeout to ensure it's ready.
+        setTimeout(function() {
+          $($('.player-songList > li')[0]).trigger('click');
+        }, 3300);
       }
     });
-    app.initPlayControls($('.main-container'), app);
-    //Sound takes a while to load, so we use the setTimeout to ensure it's ready.
-    setTimeout(function() {
-      $($('.player-songList > li')[0]).trigger('click');
-    }, 3000);
+
   });
 });
 
