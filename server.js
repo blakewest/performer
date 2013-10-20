dc9ab205af22bc475e4fd318636606e204fa95e5
Blake West <bwest87@gmail.com>
2013-10-20 15:44:53 -0700

    Made first letter capitalized on these commits

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var server = express();
 var env = process.env.NODE_ENV || 'development'
 var config = require('./config/config.js')[env];
 
-//sets up the static directory to serve from public. 
-//side note: this automatically matches a get request of '/' to index.html. 
+//Sets up the static directory to serve from public.
+//Side note: this automatically matches a get request of '/' to index.html.
 server.configure(function() {
   server.use(express.static(__dirname + '/public'));
 });
 
-//calls our routes file and passes the server and config object.
+//Calls our routes file and passes the server and config object.
 require('./routes.js').routes(server, config);
 
 var port = process.env.PORT || config.port || 3000;
